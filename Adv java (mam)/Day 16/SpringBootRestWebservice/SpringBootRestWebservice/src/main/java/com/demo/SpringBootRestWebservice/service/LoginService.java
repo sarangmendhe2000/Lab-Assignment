4db088c74dd09f39db5ce351de4f8fe9afc7be81
Sarang <sarangmendhe22@gmail.com>
2025-12-12 17:39:52 +0530

    Full stack with jwt

--- Adv java (mam)/Day 16/SpringBootRestWebservice/SpringBootRestWebservice/src/main/java/com/demo/SpringBootRestWebservice/service/LoginService.java
+++
@@ -1,9 +0,0 @@
-package com.demo.SpringBootRestWebservice.service;
-
-import com.demo.SpringBootRestWebservice.beans.MyUSer;
-
-public interface LoginService {
-
-	MyUSer validateUser(String uname, String passwd);
-
-}
